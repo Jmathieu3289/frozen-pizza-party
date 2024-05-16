cc3d22bbe06de08545a082cff50bb6c3ca39e8bd
Justin Mathieu <Justin.A.Mathieu@gmail.com>
2024-05-16 00:49:55 -0500

    Updating site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+const PUBLIC_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0ZmhpeHF6eWFnYWJ1bmV3cWFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4Mjc3NTEsImV4cCI6MjAzMTQwMzc1MX0.1FIRakbu5K2EnoPC-K5UhNbCpUxJoRNJ812o06tqqkk';
 
 function App() {
+
+  const supabase = createClient('https://ttfhixqzyagabunewqak.supabase.co', PUBLIC_KEY);
+
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await supabase.from('pizza').select();
+      setData(data);
+    }
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((d) => (
+        <div key={d.id}>{d.brand} - {d.flavor} - ${d.price}</div>
+      ))}
     </div>
   );
 }
