270a781bd888911079e36ce32bc73fe8cc1fd104
Justin Mathieu <Justin.A.Mathieu@gmail.com>
2025-07-14 20:56:49 -0500

    Updates

---
+++ static/js/952.fc373904.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrozen_pizza_party=self.webpackChunkfrozen_pizza_party||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.fc373904.chunk.js.map
